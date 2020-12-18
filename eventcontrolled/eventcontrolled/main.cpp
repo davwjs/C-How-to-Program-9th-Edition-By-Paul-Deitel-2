c9a22f02ef4248f5aec72aba68cc223838506510
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:29:56 -0500

    eventControlled exercise

---
+++ eventcontrolled/eventcontrolled/main.cpp
@@ -0,0 +1,14 @@
+// event controlled loop
+// end of file
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    while (cin >> x)
+        cout << "x = " << x << endl;
+    
+    return 0;
+}
+// control + d : end program in linux or mac or unix
+// control + z : end program in window

24278c0fcfab6e616c304fa7608169fed886f51d
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:28:54 -0500

    doWhile exercise

---
+++ dowhile/dowhile/main.cpp
@@ -0,0 +1,12 @@
+// do while loop is a posttest loop
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 1;
+    do{
+        cout << "x = " << x << endl;
+    } while(x++ < 5);
+    
+    return 0;
+}

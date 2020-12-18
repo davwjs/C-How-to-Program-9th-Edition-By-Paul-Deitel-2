87c63ff9a3b8eae35de6728473e9e76e366d7393
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:34:06 -0500

    function3 exercise

---
+++ function3/function3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    void change(int, int); // declaration call by value
+    int x = 10, y = 20;
+    cout << "before the call" << endl;
+    cout << "x = " << x << ",y = " << y << endl;
+    change(x, y);
+    cout << "after the call" << endl;
+    cout << "x = " << x << ",y = " << y << endl;
+    
+    return 0;
+}
+//function definition
+void change(int a, int b) { // header
+    int temp = 0;;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "in change" << endl;
+    cout << "x = " << a << ",y = " << b << endl;
+}

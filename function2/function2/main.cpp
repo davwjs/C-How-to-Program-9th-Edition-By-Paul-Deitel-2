9ab437d2a8cfb70e6d9e6ea16983dc77816f2b25
DavidWJS <37298091+DavidWJS@users.noreply.github.com>
2020-12-18 17:33:14 -0500

    function2 exercise

---
+++ function2/function2/main.cpp
@@ -0,0 +1,11 @@
+// display the date
+#include <iostream>
+using namespace std;
+
+int main() {
+    void displaydate();
+    displaydate();
+}
+void displaydate() {
+    cout << __DATE__ << endl;
+}
